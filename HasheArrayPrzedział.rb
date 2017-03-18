c24878502d0890eca9a497e2808645fc9e9f13ab
Patryk Kamelski <patryk_kamelski@interia.pl>
2017-03-18 12:24:45 +0100

    Add files via upload

---
+++ HasheArrayPrzedział.rb
@@ -0,0 +1,11 @@
+hashe = {test1: 1, test2: "Dwa", test3: 3, test4: "Cztery"}
+
+ puts hashe.keys
+ puts hashe.values
+
+ [1, 2, 3, 4].each { |d| puts "Hello po raz #{d}"
+ }
+
+print ("a".."z").to_a
+
+puts ("aa".."cc").to_a

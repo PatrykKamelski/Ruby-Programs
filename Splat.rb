f582e74ee599bf2e3aea7a9e6c90285fcc8280c8
Patryk Kamelski <patryk_kamelski@interia.pl>
2017-03-18 12:59:04 +0100

    Add files via upload

---
+++ Splat.rb
@@ -0,0 +1,15 @@
+puts "Użycie splat'a"
+puts "Tablice"
+puts a = [1,2,3,4,5]
+puts "------------"
+#komentarz b, c, *d = a
+puts b = a.first
+puts c = a[1]
+puts d = a[2..4]
+
+puts "Przedziały"
+puts x = 1..10
+print x.to_a
+puts " "
+puts "------------"
+print [*x]
